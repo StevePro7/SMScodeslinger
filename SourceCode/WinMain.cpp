ce6821adda88680a4ace9dc7d4c788232ebf6244
StevePro7 <steveprozune@hotmail.com>
2016-08-24 21:05:57 +0100

    * Update configuration.

--- SourceCode/WinMain.cpp
+++ SourceCode/WinMain.cpp
@@ -1,9 +1,7 @@
 #include <windows.h>
 
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                    PSTR szCmdLine, int iCmdShow)
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
      MessageBox (NULL, TEXT ("Hello, Windows 2016!"), TEXT ("HelloMsg"), 0) ;
-
      return 0 ;
 }
